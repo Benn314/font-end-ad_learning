5f6425c00fd9a0c51181a2acb0b98d86a331b7a2
Benn314 <209441806@qq.com>
2022-11-08 11:21:05 +0800

    update

--- mustache/SGG_TemplateEngine/src/index.js
+++ mustache/SGG_TemplateEngine/src/index.js
@@ -3,9 +3,9 @@
 // console.log(123);
 // console.log(456);
 // 下面两句是es6的写法
-import { ha } from './haha'; // 需要解构 所以ha要加{} 变成{ha}
+// import { ha } from './haha'; // 需要解构 所以ha要加{} 变成{ha}
 // import * as haha from './haha'; // 全部引入然后再去调用就行
 
 
-ha();
+// ha();
 // haha.ha(); // 这样也可以
