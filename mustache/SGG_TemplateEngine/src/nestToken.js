e371c7f2a9a36e9a3e9cc87639faaac8b72c76ad
Benn314 <209441806@qq.com>
2022-11-09 12:49:09 +0800

    tokens嵌套

--- mustache/SGG_TemplateEngine/src/nestToken.js
+++ mustache/SGG_TemplateEngine/src/nestToken.js
@@ -7,35 +7,34 @@ export default function nestedTokens(tokens) {
   var nestedTokens = [];
   // 栈结构 存放小tokens 栈顶（靠近端口的 最新进入的）的tokens数组中当前操作的这个小tokens小数组
   var sections = []; //因为是push进元素 所以队尾是栈口(length-1的索引位置) 第0项是栈底
+  // 收集器 天生指向nestedTokens结果数组 引用类型值 所以指向的是同一个数组
+  // 收集器的指向会变化 当遇见#的时候 收集器会指向这个token的下标为2的新数组
+  var collector = nestedTokens;
 
-  console.log(tokens);
+  // console.log(tokens);
 
   for (var i = 0; i < tokens.length; i++) {
     let token = tokens[i];
 
     switch (token[0]) {
       case '#':
-        // 给这个token下标为2的项创建一个数组 以收集子元素
-        token[2] = [];
-        // 压栈（入栈）
+        // 收集器中放入这个token
+        collector.push(token);
+        // 入栈
         sections.push(token);
-        // console.log(token[1] + '入栈了');
-        nestedTokens.push(token);
+        // 收集器要换人了 给token添加下标为2的项 并且让收集器指向它
+        collector = token[2] = [];
         break;
       case '/':
         // 出栈 pop()会返回刚刚弹出的项
-        let section_pop = sections.pop();
-        // 刚刚弹出的项还没有加入到结果数组中
-        nestedTokens.push(section_pop);
-        // console.log(section[1] + '出栈了');
+        sections.pop();
+        // 改变收集器为栈结构队尾（队尾是栈顶）那项的下标为2的数组
+        collector = sections.length > 0 ? sections[sections.length - 1][2] : nestedTokens;
         break;
       default:
-        // 判断 栈队列当前情况
-        if (sections.length == 0) {
-          nestedTokens.push(token);
-        } else {
-          sections[sections.length - 1][2].push(token);
-        }
+        // 甭管当前的collector是谁 可能是结果nestedTokens
+        // 也可能是某个token的下标为2的数组 甭管是谁 推入collector即可
+        collector.push(token);
     }
   }
 
--- mustache/mustache.md
+++ mustache/mustache.md
@@ -702,3 +702,61 @@ export default function parseTemplateToTokens(templateStr) {
 
 # 11 手写将tokens嵌套起来
 
+视频很棒！建议二刷
+
+运用了js的引用类型数据 定义collector收集器来跳转push/pop的位置 以便嵌套tokens
+
+代码里的 sections[] 则是数据结构-栈思想的具象化
+
+**nestToken.js**
+
+```js
+/**
+ * 函数的功能是折叠tokens 将#和/之间的tokens能够整合起来 作为他的下标为3的项
+ * @param {*} tokens 
+ */
+export default function nestedTokens(tokens) {
+  // 结果数组
+  var nestedTokens = [];
+  // 栈结构 存放小tokens 栈顶（靠近端口的 最新进入的）的tokens数组中当前操作的这个小tokens小数组
+  var sections = []; //因为是push进元素 所以队尾是栈口(length-1的索引位置) 第0项是栈底
+  // 收集器 天生指向nestedTokens结果数组 引用类型值 所以指向的是同一个数组
+  // 收集器的指向会变化 当遇见#的时候 收集器会指向这个token的下标为2的新数组
+  var collector = nestedTokens;
+
+  // console.log(tokens);
+
+  for (var i = 0; i < tokens.length; i++) {
+    let token = tokens[i];
+
+    switch (token[0]) {
+      case '#':
+        // 收集器中放入这个token
+        collector.push(token);
+        // 入栈
+        sections.push(token);
+        // 收集器要换人了 给token添加下标为2的项 并且让收集器指向它
+        collector = token[2] = [];
+        break;
+      case '/':
+        // 出栈 pop()会返回刚刚弹出的项
+        sections.pop();
+        // 改变收集器为栈结构队尾（队尾是栈顶）那项的下标为2的数组
+        collector = sections.length > 0 ? sections[sections.length - 1][2] : nestedTokens;
+        break;
+      default:
+        // 甭管当前的collector是谁 可能是结果nestedTokens
+        // 也可能是某个token的下标为2的数组 甭管是谁 推入collector即可
+        collector.push(token);
+    }
+  }
+
+  return nestedTokens;
+}
+
+```
+
+​	
+
+# 12 手写将tokens注入数据
+
